834f8082c6e14c7d7e65dadaaca610e21a5c19a4
teshanishahurdhika078-hash <teshanishahurdhika078@gmail.com>
2026-07-13 20:36:53 +0530

    Day14🖋️

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { Route } from 'react-router-dom';
 import './App.css'
+import AdminPage from './pages/adminPage';
+import HomePage from './pages/homePage';
 import ProductCard from './components/productCard';
 import TrendingProducts from './components/trendingProducts';
 
 function App() {
   return (
     <>
-      <div>
+      <div className ="w-full h-screen border-[6px] flex justify-center items-center">
+      <Routes>
+     <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/products" element={<productPage />} />
 
-       <TrendingProducts/>  
-          
-      </div>
+      </Routes>
+
+
+        </div>
+    
     </>
+    
   );
 }
 
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+    return(
+        <div className="w-full h-screen border-[6-px] flex justify-center items-center">
+            <h1 className="text-4xl font-bold" ></h1>
+            
+        </div>
+    )
+}
